009d1d2ebd49f6582af2f037397f8421d36faceb
kmsibtain <msibtain623@gmail.com>
2025-03-21 23:06:48 +0500

    RegisterHostel ApproveApplicants

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,8 @@ import RentalView from "./components/rental-view";
 import AddRooms from "./components/AddRooms";
 import ApplyRental from "./components/apply-rental";
 import ApplyParking from "./components/applyParking";
+import ApproveApplicants from "./components/ApproveApplicants";
+import RegisterHostel from "./components/register-hostel";
 import "./App.css";
 
 function App() {
@@ -23,6 +25,8 @@ function App() {
           <Route path="/addRooms" element={<AddRooms />} />
           <Route path="/apply-rental" element={<ApplyRental />} />
           <Route path="/applyParking" element={<ApplyParking />} />
+          <Route path="/ApproveApplicants" element={<ApproveApplicants />} />
+          <Route path="/register-hostel" element={<RegisterHostel />} />
         </Routes>
       </Router>
     </div>
